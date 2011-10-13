bb4c4a12900d0a2508c61b6394b445aa07f0c64a
Tiaan Kruger <tiaank@gmail.com>
2011-10-13 13:05:21 -0400

    added upgrade script

---
+++ resources/sql/1_0_x_to_1_1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `admin_branchlog` (
+    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `lastlog` varchar(20000) NOT NULL,
+    `branch_id` integer NOT NULL
+)
+;
+ALTER TABLE `admin_branchlog` ADD CONSTRAINT `branch_id_refs_id_5f90c3fe` FOREIGN KEY (`branch_id`) REFERENCES `admin_branch` (`id`);
+
+alter table `admin_branch` add column `run_status` varchar(1) NOT NULL default 'u';
